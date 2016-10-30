6abe64a91e103795c24e311f02e2f4cfc8b54271
Tomas Divis <tomas.divis@nic.cz>
2016-10-30 21:53:17 +0100

    Enlagre font to 14.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+char font[] = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 int borderpx = 2;
 
 /*
